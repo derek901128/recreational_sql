e29a4c23001c0764eb05ea0cf70aa2404a35db3b
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-07-12 21:31:36 +0800

    Update num_to_binary.sql

--- num_to_binary.sql
+++ num_to_binary.sql
@@ -2,23 +2,26 @@ with recursive num_to_binary(
 	cur_num,
 	ibit,
 	ibinary,
-	next_num
+	next_num,
+	bin_cnt
 ) as (
 	select 
 		( select v from param ), 
 		mod(( select v from param ), 2), 
 		mod(( select v from param ), 2)::integer::text, 
-		floor( (select v from param)  / 2)
+		floor( (select v from param)  / 2),
+		1
 	union all
 	select 
 		next_num, 
 		mod(next_num, 2),  
 		mod(next_num, 2)::integer::text || ibinary, 
-		floor(next_num / 2)
+		floor(next_num / 2),
+		bin_cnt + 1
 	from 
 		num_to_binary
 	where 
 		next_num > 0
 ),
 param(v) as ( select 12 )
-select ibinary from num_to_binary where next_num = 0;
+select ibinary, bin_cnt from num_to_binary where next_num = 0;
