40dfd4425e6c780114a9cac6c2c36c73c0400bae
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-08-29 00:17:34 +0800

    Update handrolled_repeat_str_with_delimiter.sql

--- handrolled_repeat_str_with_delimiter.sql
+++ handrolled_repeat_str_with_delimiter.sql
@@ -1,6 +1,7 @@
 with 
 "parameters"(input_str, repeat_times, delim) as (select 'derek', 7, '|' from dual),
-"output"(output_str) as (
+"output"(output_str) as 
+(
 	select 
     	rpad(input_str, length(delim || input_str) * repeat_times - 1, delim || input_str) 
     from 
