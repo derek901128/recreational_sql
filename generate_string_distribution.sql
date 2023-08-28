0c69d5be51eb773cd7195c8c17979cc31a3d10d7
derek901128 <96558860+derek901128@users.noreply.github.com>
2023-08-29 00:14:20 +0800

    Update generate_string_distribution.sql

--- generate_string_distribution.sql
+++ generate_string_distribution.sql
@@ -1,6 +1,7 @@
 with 
 para (w_cnt, l_cnt) as( select 100, 500 from dual ),
-generate_letters(word_id, letter_lvl, letter) as (
+generate_letters(word_id, letter_lvl, letter) as 
+(
     select 
         floor(dbms_random.value(1, ( select w_cnt from para ))),
         level,
@@ -10,7 +11,8 @@ generate_letters(word_id, letter_lvl, letter) as (
     connect by 
         level <= ( select l_cnt from para )
 ),
-form_words as (
+form_words as 
+(
     select 
         word_id,
         listagg(letter) within group(order by letter_lvl) as word,
@@ -20,7 +22,8 @@ form_words as (
     group by
         word_id
 ),
-len_distrib as (
+len_distrib as 
+(
     select 
         word_len,
         count(word_len) as len_freq,
